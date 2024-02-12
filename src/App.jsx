bbbbb8331aecf49cac7f3529560229da491904cc
SandyFit <sandy07r@gmail.com>
2024-02-12 18:14:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
+    
+
+
+
+const App = () => {
+
+    const [step, setStep] = useState(1);
+    const [isActive, setIsActive] = useState(false);
+
+    const handlePrevious = () => {
+        if (step > 1) setStep(step - 1);
+    }
+
+    const handleNext = () => {
+         if (step < 3) setStep(step + 1);
+    }
+
+    return (
+        <div className='steps'>
+            <div className="numbers">
+                <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+                <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+                <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+            </div>
+
+            <p className="message">
+                Step {step}: {messages[step - 1]}
+            </p>
+
+            <div className="buttons">
+                <button 
+                    style={{backgroundColor: '#7950f2', color: '#f2f2f2', cursor: 'pointer'}}
+                    onClick={handlePrevious}
+                    
+                >
+                    Previous
+                </button>
+                <button 
+                    style={{ backgroundColor: '#7950f2', color: '#f2f2f2', cursor: 'pointer' }}
+                    onClick={handleNext}
+                    
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default App
